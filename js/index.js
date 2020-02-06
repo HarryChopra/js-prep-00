6636f91c099cc7076a1d5e43f8865726ae7ce14f
Harry Chopra <harrychopra@gmail.com>
2020-02-06 11:52:21 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-var name = "Joe"
+var name = "Mark"
 var height = "74"
 
 // Don't worry about this
 module.exports = { name, height
 }
 
+// name === "Susan" ? console.log("The name is correct") : console.log("Expected the name to be Susan")
--- other_file.js
+++ other_file.js
@@ -1,1 +1,2 @@
-console.log(name)
+let index = require("./js/index")
+console.log(index.name)
